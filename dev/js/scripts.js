cd7e27131a62aacaa63904510b3ccde6be1d4edc
caseyreinknecht <81766422+caseyreinknecht@users.noreply.github.com>
2022-05-23 14:01:55 -0400

    1

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,8 @@
-// import { gsap } from "gsap";
+ import { gsap } from "gsap";
+ import { GSDevTools } from "gsap/GSDevTools";
+ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+ 
+
+ const mainTL = gsap.timeline();
+
+
