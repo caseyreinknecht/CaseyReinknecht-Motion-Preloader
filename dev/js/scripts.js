04c3ece36fa87a92d0de848e2dcf941ddae12295
caseyreinknecht <81766422+caseyreinknecht@users.noreply.github.com>
2022-05-24 16:24:14 -0400

    all work

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,48 @@
  import { gsap } from "gsap";
- import { GSDevTools } from "gsap/GSDevTools";
- import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
  
 
- const mainTL = gsap.timeline();
+ gsap.registerPlugin(DrawSVGPlugin);
+ 
+
+ const mainTL = gsap.timeline({id:"mainTL"});
+
+ function rindMotion(){
+    let tl = gsap.timeline();
+
+    tl.from("#rind-left", {duration:2, drawSVG:0}, "rind")
+    .from("#rind-right", {duration:2, drawSVG:0}, "rind")
+
+    ;
+
+    }
+
+    
+
+    
+
+    function wedgeFill(){
+
+        let tl = gsap.timeline();
+
+        tl.from(".wedge", {duration:1.5, stagger:0.7, ease: "power1.inOut", opacity:0 }, "start")
+
+       ;
+
+        
+    }
+
+    
+
+   
+
+    
+    
+
+ 
 
 
+
+ mainTL.add(rindMotion())
+ .add(wedgeFill())
+ ;
